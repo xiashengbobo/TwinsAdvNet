3b4dd1c4fa36d1c04453b3cd9eaeb3bea31682a8
xiashengbobo <wanghaixingbo@gmail.com>
2018-11-17 17:26:53 +0800

    Add files via upload

---
+++ datasets/voc_dataset.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  5 21:24:31 2018
+
+@author: bobo
+"""
+
+import os
+import os.path as osp
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import collections
+
+import torch
+import torchvision
+
+import cv2
+from torch.utils import data
+from PIL import Image
+
